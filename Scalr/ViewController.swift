da0a5fa7cb4980894b1f45fa136248c33f4463bf
Mike Heft <mikeheft@gmail.com>
2023-02-03 18:51:33 -0700

    Initial Commit

---
+++ Scalr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scalr
+//
+//  Created by Michael Heft on 2/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
